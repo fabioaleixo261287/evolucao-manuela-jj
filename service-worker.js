4d95b7913d7129fe0341170771bcd336d4c0373b
Fabio Aleixo <fabio.aleixo@dedalus.eu>
2026-07-22 15:00:48 -0300

    Melhora performance e sincronizacao

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "alliance-mooca-kids-pwa-v8";
+const CACHE_NAME = "alliance-mooca-kids-pwa-v9";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -18,7 +18,9 @@ const APP_SHELL = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+    caches.open(CACHE_NAME).then((cache) =>
+      Promise.allSettled(APP_SHELL.map((url) => cache.add(url)))
+    )
   );
   self.skipWaiting();
 });
@@ -56,7 +58,7 @@ self.addEventListener("fetch", (event) => {
           caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
         }
         return response;
-      });
+      }).catch(() => caches.match("./index.html"));
     })
   );
 });
